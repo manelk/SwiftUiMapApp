759f2c8d3aefb079e4d5c6f9340886455de2c504
manel kacem <manelkacem11@gmail.com>
2024-12-11 18:01:51 +0100

    Initial Commit

---
+++ SwiftUiMapApp/SwiftUiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapAppApp.swift
+//  SwiftUiMapApp
+//
+//  Created by odc on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
